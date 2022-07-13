79ef654f28a30c8623e2996ed6fa734d60c47609
Abhishek Chandar <achan260@uottawa.ca>
2022-07-13 18:29:01 -0400

    Added YOLO model weights

--- save_model.py
+++ save_model.py
@@ -4,8 +4,9 @@ from absl.flags import FLAGS
 from core.yolov4 import YOLO, decode, filter_boxes
 import core.utils as utils
 from core.config import cfg
+import os
 
-flags.DEFINE_string('weights', './data/yolov4.weights', 'path to weights file')
+flags.DEFINE_string('weights', "./data/yolov4.weights", 'path to weights file')
 flags.DEFINE_string('output', './checkpoints/yolov4-416', 'path to output')
 flags.DEFINE_boolean('tiny', False, 'is yolo-tiny or not')
 flags.DEFINE_integer('input_size', 416, 'define input size of export model')
